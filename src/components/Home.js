b07f1e0394815b4b9ecc45d32a8cb861a63ef043
Sawyer Pearson <sawyer.pearson92@gmail.com>
2022-06-25 17:09:08 -0600

    added spotify and youtube media

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 function Home() {
-    return (
-      <div>
-        some text <br/>
-        some more text <br/>
-        and some more <br/>
+    return (<>
+    <div className='home'>
+      <div className='home-media'>
+        <div className='spotify-player'>
+            <iframe src="https://open.spotify.com/embed/artist/0dFzZO6uuLyEyPFyd2aWRT?utm_source=generator" width="100%" height="380"></iframe>
+        </div>
+          <div className='youtube-player'>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/S580Zf_D8jM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+            </iframe>
+          </div>
       </div>
-    );
+        
+    </div>
+
+    </>);
   }
   
   export default Home;
