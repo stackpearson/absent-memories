fdd5864d77eec69798e083a8b21a5aa42286cf66
Sawyer Pearson <sawyer.pearson92@gmail.com>
2022-06-25 18:22:33 -0600

    about done

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,7 @@ function Home() {
         </div>
         <div className='youtube-player'>
             <h2>Where Were You Music Video</h2>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/S580Zf_D8jM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/S580Zf_D8jM" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
             </iframe>
         </div>
       </div>
